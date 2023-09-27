3d5b5038dd4d8cc27b4a04026804ae31a539cff6
DavidTram0311 <90774831+DavidTram0311@users.noreply.github.com>
2023-09-27 22:16:08 +0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -14,6 +14,7 @@ def stop(count):
     if count % 50 == 0:
         time.sleep(60)
 
+# Create dict for model 3 - run for gcs tool
 def m_dict_3(cls, ci):
     my_dict = {}
     awb = []
@@ -36,16 +37,6 @@ def m_dict_3(cls, ci):
 def average(lst):
     return sum(lst) / len(lst)
 
-def transform_dict(my_dict):
-    for i in range(0,4):    
-        if str(my_dict[i]) == '[]':
-            my_dict[i] = [0]
-
-    for i in range(0,4):
-        my_dict[i] = average(my_dict[i])
-    
-    return my_dict
-
 def transform_dict_3(my_dict):
     for i in range(0,3):    
         if str(my_dict[i]) == '[]':
@@ -56,19 +47,39 @@ def transform_dict_3(my_dict):
     
     return my_dict
 
-# def m_dict_5(cls, ci):
-#     my_dict = {}
-#     awb = []
-#     bulky = []
-#     small = []
-#     awb_njv = []
+#---------------------------------------------------------------------------------------------------------#
 
-#     for i in range(len(cls)):
-#         if cls[i] == 0:
-#             awb.append(ci[i])
-#         elif cls[i] == 1:
-#             awb_njv.append(ci[i])
-#         elif cls[i] == 2:
-#             bulky.append(ci[i])
-#         elif cls[i] == 3:
-#             small.append(ci[i])
+# Create dict for model 5 - run for gcs tool
+def m_dict_5(cls, ci):
+    my_dict = {}
+    awb = []
+    bulky = []
+    small = []
+    awb_njv = []
+
+    for i in range(len(cls)):
+        if cls[i] == 0:
+            awb.append(ci[i])
+        elif cls[i] == 1:
+            awb_njv.append(ci[i])
+        elif cls[i] == 2:
+            bulky.append(ci[i])
+        elif cls[i] == 3:
+            small.append(ci[i])
+    
+    my_dict[0] = awb
+    my_dict[1] = awb_njv
+    my_dict[2] = bulky
+    my_dict[3] = small
+
+    return my_dict
+
+def transform_dict_5(my_dict):
+    for i in range(0,4):    
+        if str(my_dict[i]) == '[]':
+            my_dict[i] = [0]
+
+    for i in range(0,4):
+        my_dict[i] = average(my_dict[i])
+    
+    return my_dict
